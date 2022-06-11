7e8022b678d3f7ca5b99688ae6b0cc437918e147
Swapnil Tiwari <swapnil_tiwari@outlook.com>
2022-06-11 12:51:56 +0530

    dynamic msgs

--- src/App.js
+++ src/App.js
@@ -6,27 +6,15 @@ import FormView3 from "./components/FormView/FormView3";
 import MessageView from "./components/MessageView/MessageView";
 import HeaderView from "./components/Header/HeaderView";
 import ProgressBar from "./components/ProgressBar/ProgressBar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 function App() {
-  let msgsLarge = {
-    msg1: "Welcome! First things first",
-    msg2: "Let's set up a home for all your work",
-  };
-  let msgsSmall = {
-    msg1: "You can always change them later.",
-    msg2: "You can always create another workspace later.",
-  };
-  let msgState = msgsLarge.msg1;
-  console.log(msgState);
-  let msgSmall = msgsSmall.msg1;
   return (
     <div className="App">
       <BrowserRouter>
         <HeaderView />
         <ProgressBar />
         <div className={`container styledContainer`}>
-          <MessageView msgLarge={msgState} msgSmall={msgSmall} />
-
+          <MessageView />
           <Routes>
             <Route index element={<FormView />} />
             <Route path="formView2" element={<FormView2 />} />
--- src/components/MessageView/MessageView.js
+++ src/components/MessageView/MessageView.js
@@ -1,15 +1,33 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import styles from './message.module.css'
+import React from "react";
+import PropTypes from "prop-types";
+import styles from "./message.module.css";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 function MessageView(props) {
+  const location = useLocation();
+  console.log(location.pathname);
+  let paths = {
+    "/": 1,
+    "/formView2": 2,
+    "/formView3": 3,
+  };
+  let msgsLarge = {
+    "/": "Welcome! First things first",
+    "/formView2": "Let's set up a home for all your work",
+  };
+  let msgsSmall = {
+    "/": "You can always change them later.",
+    "/formView2": "You can always create another workspace later.",
+  };
+  let msgState = msgsLarge[location.pathname];
+  let msgSmall = msgsSmall[location.pathname];
   return (
     <div className={styles.messageContainer}>
-        <h2>{props.msgLarge}</h2>
-        <p>{props.msgSmall}</p>
+      <h2>{msgState}</h2>
+      <p>{msgSmall}</p>
     </div>
-  )
+  );
 }
 
-MessageView.propTypes = {}
+MessageView.propTypes = {};
 
-export default MessageView
+export default MessageView;
